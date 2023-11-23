024f5c5c008a02a84357ac9c3890e1e051f928ca
albertsalendah@gmail.com <ertsalendah@gmail.com>
2023-11-23 12:22:12 +0700

    fix chat

--- routes/message.js
+++ routes/message.js
@@ -26,6 +26,8 @@ admin.initializeApp({
 router.post("/send_message", async (req, res) => {
   try {
     const {
+      id_message,
+      conversation_id,
       id_sender,
       id_receiver,
       tanggal_kirim,
@@ -45,16 +47,17 @@ router.post("/send_message", async (req, res) => {
         .jpeg({ quality: 70 })
         .toBuffer();
       compressedBase64 = compressedBuffer.toString("base64");
-      console.log("Image compressed successfully");
+      //console.log("Image compressed successfully");
     } else {
       compressedBase64 = image;
     }
     const query =
-      "INSERT INTO `messages`(`id_message`, `id_sender`, `id_receiver`, `tanggal_kirim`, `jam_kirim`, `message`, `image`, `messageRead`) VALUES (?,?,?,?,?,?,?,?)";
+      "INSERT INTO `messages`(`id_message`, `conversation_id`,`id_sender`, `id_receiver`, `tanggal_kirim`, `jam_kirim`, `message`, `image`, `messageRead`) VALUES (?,?,?,?,?,?,?,?,?)";
     db.query(
       query,
       [
-        makeid(32),
+        id_message,
+        conversation_id,
         id_sender,
         id_receiver,
         tanggal_kirim,
@@ -76,7 +79,6 @@ router.post("/send_message", async (req, res) => {
           data: { senderID: id_receiver, receiverID: id_sender },
           token: fcm_token,
         };
-
         await admin
           .messaging()
           .send(messages)
@@ -112,79 +114,27 @@ router.get("/get_list_health_worker", async (req, res) => {
   }
 });
 
-// router.get("/get_latest_self_message", async (req, res) => {
-//   try {
-//     const { userID } = req.body;
-//    const query =
-//      "SELECT m.* FROM messages m JOIN ( SELECT id_sender, MAX(tanggal_kirim) AS max_tanggal_kirim FROM messages WHERE id_sender = ? GROUP BY id_receiver ) latest ON m.id_sender = latest.id_sender AND m.tanggal_kirim = latest.max_tanggal_kirim ORDER BY m.tanggal_kirim DESC;";
-//    db.query(query, userID, (error, results) => {
-//       if (error) {
-//         console.error(error);
-//         return;
-//       }
-//       res.status(200).json(results);
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: error });
-//   }
-// });
-
 router.get("/get_latest_self_message", async (req, res) => {
   try {
     const { userID } = req.body;
+    const query = `SELECT m.*, DATE_FORMAT(m.tanggal_kirim, '%Y-%m-%d %H:%i:%s') AS tanggal_kirim
+    FROM messages m
+    JOIN (
+      SELECT conversation_id, MAX(tanggal_kirim) AS max_date
+      FROM messages
+      WHERE conversation_id LIKE ?
+      GROUP BY conversation_id
+    ) latest
+    ON m.conversation_id = latest.conversation_id AND m.tanggal_kirim = latest.max_date
+    ORDER BY m.tanggal_kirim DESC;`;
 
-    const messagesQuery =
-      "SELECT m.* FROM messages m JOIN ( SELECT id_sender, MAX(tanggal_kirim) AS max_tanggal_kirim FROM messages WHERE id_sender = ? GROUP BY id_receiver ) latest ON m.id_sender = latest.id_sender AND m.tanggal_kirim = latest.max_tanggal_kirim ORDER BY m.tanggal_kirim DESC;";
-
-    db.query(messagesQuery, userID, (error, messagesResults) => {
+    db.query(query, [`%${userID}%`], (error, messagesResults) => {
       if (error) {
         console.error(error);
         res.status(500).json({ error: error });
         return;
       }
-
-      // Extract id_receiver values from the messagesResults
-      const idReceivers = messagesResults.map((message) => message.id_receiver);
-
-      if (!idReceivers || idReceivers.length === 0) {
-        // Handle the case where idReceivers is null or empty
-        res.status(200).json([]);
-        return;
-      }
-
-      const usersQuery =
-        "SELECT userID, nama AS namaReceiver, keterangan AS ketReceiver, fcm_token AS fcm_token,foto AS fotoReceiver FROM users WHERE userID IN (?)";
-
-      db.query(usersQuery, [idReceivers], (error, usersResults) => {
-        if (error) {
-          console.error(error);
-          res.status(500).json({ error: error });
-          return;
-        }
-
-        // Combine message results with user information based on id_receiver
-        const combinedResults = messagesResults.map((message) => {
-          const correspondingUser = usersResults.find(
-            (user) => user.userID === message.id_receiver
-          );
-          return {
-            ...message,
-            namaReceiver: correspondingUser
-              ? correspondingUser.namaReceiver
-              : null,
-            ketReceiver: correspondingUser
-              ? correspondingUser.ketReceiver
-              : null,
-            fcm_token: correspondingUser ? correspondingUser.fcm_token : null,
-            fotoReceiver: correspondingUser
-              ? correspondingUser.fotoReceiver
-              : null,
-          };
-        });
-
-        res.status(200).json(combinedResults);
-      });
+      res.status(200).json(messagesResults);
     });
   } catch (error) {
     console.error(error);
@@ -192,61 +142,19 @@ router.get("/get_latest_self_message", async (req, res) => {
   }
 });
 
-router.get("/get_latest_self_message_doc", async (req, res) => {
+router.get("/get_data_user_message", async (req, res) => {
   try {
     const { userID } = req.body;
 
-    const messagesQuery =
-      "SELECT m.* FROM messages m JOIN ( SELECT id_sender, MAX(tanggal_kirim) AS max_tanggal_kirim FROM messages WHERE id_receiver = ? GROUP BY  id_sender) latest ON m.id_sender = latest.id_sender AND m.tanggal_kirim = latest.max_tanggal_kirim ORDER BY m.tanggal_kirim DESC;";
+    const placeholders = userID.map(() => "?").join(",");
+    const query = `SELECT * FROM users WHERE userID IN (${placeholders})`;
 
-    db.query(messagesQuery, userID, (error, messagesResults) => {
+    db.query(query, userID, (error, results) => {
       if (error) {
         console.error(error);
-        res.status(500).json({ error: error });
-        return;
+        return res.status(500).json({ error: error });
       }
-
-      // Extract id_receiver values from the messagesResults
-      const idSender = messagesResults.map((message) => message.id_sender);
-
-      if (!idSender || idSender.length === 0) {
-        // Handle the case where idReceivers is null or empty
-        res.status(200).json([]);
-        return;
-      }
-
-      const usersQuery =
-        "SELECT userID, nama AS namaReceiver, keterangan AS ketReceiver, fcm_token AS fcm_token,foto AS fotoReceiver FROM users WHERE userID IN (?)";
-
-      db.query(usersQuery, [idSender], (error, usersResults) => {
-        if (error) {
-          console.error(error);
-          res.status(500).json({ error: error });
-          return;
-        }
-
-        // Combine message results with user information based on id_receiver
-        const combinedResults = messagesResults.map((message) => {
-          const correspondingUser = usersResults.find(
-            (user) => user.userID === message.id_sender
-          );
-          return {
-            ...message,
-            namaReceiver: correspondingUser
-              ? correspondingUser.namaReceiver
-              : null,
-            ketReceiver: correspondingUser
-              ? correspondingUser.ketReceiver
-              : null,
-            fcm_token: correspondingUser ? correspondingUser.fcm_token : null,
-            fotoReceiver: correspondingUser
-              ? correspondingUser.fotoReceiver
-              : null,
-          };
-        });
-
-        res.status(200).json(combinedResults);
-      });
+      res.status(200).json(results);
     });
   } catch (error) {
     console.error(error);
@@ -254,27 +162,39 @@ router.get("/get_latest_self_message_doc", async (req, res) => {
   }
 });
 
-router.get("/get_individual_message", async (req, res) => {
+router.post("/delete_conversation", async (req, res) => {
   try {
-    const { senderID, receiverID } = req.body;
-    const query = `SELECT * FROM messages WHERE (id_sender = ? AND id_receiver = ?) OR (id_sender = ? AND id_receiver = ?) ORDER BY tanggal_kirim DESC;`;
-
-    db.query(
-      query,
-      [senderID, receiverID, receiverID, senderID],
-      (error, results) => {
-        if (error) {
-          console.error(error);
-          res.status(500).json({ error: error });
-          return;
-        }
-
-        res.status(200).json(results);
+    const { conversation_id } = req.body;
+    const queryAkun = "DELETE FROM `messages` WHERE conversation_id = ?;";
+    db.query(queryAkun, conversation_id, (error, results) => {
+      if (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred" });
+        return;
       }
-    );
+      res.status(200).json({ message: "Chat Berhasil Dihapus" });
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: error });
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.post("/delete_single_chat", async (req, res) => {
+  try {
+    const { id_message } = req.body;
+    const queryAkun = "DELETE FROM `messages` WHERE id_message = ?;";
+    db.query(queryAkun, id_message, (error, results) => {
+      if (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred" });
+        return;
+      }
+      res.status(200).json({ message: "Chat Berhasil Dihapus" });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
   }
 });
 
